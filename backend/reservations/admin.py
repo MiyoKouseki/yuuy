c77cd569c995f478cfddfbea8680c0c73687f70f
MiyoKouseki <kouseki.miyo@gmail.com>
2023-10-11 17:40:25 +0900

    feat: やり直し。

--- backend/reservations/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Reservation, Organization, Room
-
-admin.site.register(Reservation)
-admin.site.register(Room)
-admin.site.register(Organization)
